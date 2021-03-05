93781b0dbbdf93f31b336902467358bfb589885b
Juan Chavez <jchavez666@gmail.com>
2021-03-05 17:44:21 +0200

    animations

--- 14-ANIMACIONES CON SCROLL/js/js.js
+++ 14-ANIMACIONES CON SCROLL/js/js.js
@@ -1,11 +1,13 @@
 let images = Array.from(document.querySelectorAll('img'));
+
 let imagesScrollTop = images.map( image => 
       image.getBoundingClientRect().top - (innerHeight/2))
 
 console.log(imagesScrollTop);
 
 
-// !ESCUCHAMOS EL SCROLL
+// !------------ ESCUCHAMOS EL SCROLL --------------------
+
 window.addEventListener('scroll', () => {
   imagesScrollTop.forEach( (el,i) => {
     if (scrollY >= el) {
